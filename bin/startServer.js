8c970e2f85d72c9d05c0c4bceca868261418e1aa
bgibbons0531 <bradley0531@msn.com>
2019-02-24 17:50:28 -0600

    Remove dotenv

--- bin/startServer.js
+++ bin/startServer.js
@@ -1,12 +1,3 @@
 #!/usr/bin/env node
-const path = require('path');
-const dotenv = require('dotenv');
-
-const file = path.join(__dirname, '..', '.env');
-dotenv.config({
-  silent: process.env.NODE_ENV === 'production',
-  path: file
-});
-
 console.log('Starting server...');
 
