e6078d72b8a573c67a3f20f03a1806960357dbf4
bgibbons0531 <bradley0531@msn.com>
2019-03-17 14:24:31 -0500

    Add a favicon!

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const PORT = process.env.PORT || 5000;
 
 express()
+  .use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')))
   .set('views', path.join(__dirname, '..', 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
